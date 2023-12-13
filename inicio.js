3ec97d4938ca97d446ea0dc05a824983de46a144
Romina M <140033426+rominamourelle@users.noreply.github.com>
2023-12-13 20:41:28 -0300

    Update seguda entrega

--- inicio.js
+++ inicio.js
@@ -1,20 +1,26 @@
+const fs = require("fs").promises;
+
+
 class ProductManager {
 
   static ultid = 0;
 
-    constructor() {
+    constructor(path) {
       this.productos = [];
+      this.path = path; 
+
     }
   
-    addProduct(titulo, descripcion, precio, imagen, codigo, stock) {
+    async addProduct(nuevoObjeto) {
+      let {titulo, descripcion, precio, imagen, codigo, stock} = nuevoObjeto;
 
-        if(!titulo || !descripcion || !precio || !imagen || !stock)
-        {
+
+        if (!titulo || !descripcion || !precio || !imagen || !stock) {
           console.log("Completar todos los datos"); 
           return;
         }
 
-        if(this.productos.some(item => item.codigo === codigo))
+        if (this.productos.some(item => item.codigo === codigo))
         {
           console.log("Los codigos son unicos"); 
           return;
@@ -31,40 +37,142 @@ class ProductManager {
         }
 
         this.productos.push(nuevoproducto);
-      }
-      
-        getProductos()  {
+        await this.guardarArchivo(this.productos);
+    }
+    
+  }
+
+        getProductos();  {
           console.log(this.productos);
         }
 
-        getProductosporId(id) {
-          const productos = this.productos.find(item => item.id === id);
+        async getProductosporId(id); {
+          try {
+            const arrayProductos = await this.leerArchivo();
+            const buscado = arrayProductos.find(item => item.id === id);
 
-          if(!productos) {
-            console.log("Producto no encontrado")
-          } else {
-            console.log("Producto encontrado", productos); 
+            if (!buscado) {
+              console.log("producto no encontrado");
+             } else {
+              console.log("Producto encontrado");
+              return buscado;
+              
             }
-            return productos;
+
+          }  catch (error) {
+            console.log("No se puede leer el archivo", error);
+
           }
-        }
+
+          }
+
+
+
+
+           async leerArchivo(); {
+
+            try {
+              const respuesta = await fs.readfile(this.path, "utf-8"); 
+              const arrayProductos = JSON. parse (respuesta);
+              return arrayProductos; 
+              
+              } 
+              catch (error)  {
+                  console.log("No se puede leer el archivo", error);
+            }
+
+          }
+
+            async guardarArchivo(arrayProductos); {
+            try {
+              await fs.writeFile(this.path, JSON.stringify(arrayProductos, null, 2));
+            } catch (error) {
+              console.log("No se puede leer el archivo", error); 
+            }
+
+            async updateProduct(id, productoActualizado); {
+              try {
+                  const arrayProductos = await this.leerArchivo();
+      
+                  const index = arrayProductos.findIndex(item => item.id === id);
+      
+                  if (index !== -1) {
+                      arrayProductos.splice(index, 1, productoActualizado);
+                      await this.guardarArchivo(arrayProductos);
+                  } else {
+                      console.log("Producto no encontrado");
+                  }
+      
+              } catch (error) {
+                  console.log("Error al actualizar el producto", error);
+              }
+          }
+
+          }
 
   
-//  Testing 
 
-      const manager = new ProductManager(); 
+    //nuevo testing
 
-      manager.getProductos();
+     const manager = new ProductManager("./productos.json"); 
 
-      manager.addProduct("Producto prueba", "prueba de producto", 300, "no hay imagen", "abc123", 30)
-
-      manager.addProduct("arroz", "prueba de producto", 300, "no hay imagen", "abc124", 30)
-
-      manager.addProduct("fideos", "prueba de producto", 300, "no hay imagen", "abc125", 30)
-
-      anager.getProductosporId(2);
-
-     manager.getProductosporId(22);
+     manager.getProductos();
 
 
+     const fideos = {
+      titulo: "fideos",
+      descripcion: "super ricos",
+      precio: 200, 
+      imagen: "sin imagen",
+      codigo: "abc123",
+      stock: 20, 
 
+     }
+
+     manager.addProduct(fideos);
+
+
+     const arroz = {
+      titulo: "arroz",
+      descripcion: "no se pasa",
+      precio: 150, 
+      imagen: "sin imagen",
+      codigo: "abc124",
+      stock: 10,
+    }
+
+    manager.addProduct(arroz); 
+
+    const aceite = {
+      title: "aceite",
+      description: "muy caro",
+      price: 8000,
+      img: "sin imagen",
+      code: "abc125",
+      //stock: 30
+    }
+
+    manager.getProductos();
+
+    async function testeamosBusquedaPorId() {
+      const buscado = await manager.getProductById(2);
+      console.log(buscado);
+  }
+
+  testeamosBusquedaPorId();
+
+  const salsa = {
+    id: 1,
+    title: "salsa tomate", 
+    description: "muy dulce", 
+    price: 190,
+    img: "Sin imagen",
+    code: "abc123",
+    stock: 15
+};
+
+async function testeamosActualizar() {
+  await manager.updateProduct(1, salsa);
+}
+
+testeamosActualizar();
